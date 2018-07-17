d27a94ab810d0b929724a9da3954722a8b05e27f
ZeroBounce <34064209+zerobounce@users.noreply.github.com>
2018-07-17 14:32:55 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,44 +10,26 @@ def zerobounce():
 @pytest.fixture
 def zerobounce_response_validate():
     return {
-        "address": "flowerjill@aol.com",
-        "status": "Valid",
-        "sub_status": "",
-        "account": "flowerjill",
-        "domain": "aol.com",
-        "disposable": False,
-        "toxic": False,
-        "firstname": "Jill",
-        "lastname": "Stein",
-        "gender": "female",
-        "location": None,
-        "creationdate": None,
-        "processedat": "2017-04-01 02:48:02.592"
-    }
-
-
-@pytest.fixture
-def zerobounce_response_validatewithip():
-    return {
-        "address": "flowerjill@aol.com",
-        "status": "Valid",
-        "sub_status": "",
-        "account": "flowerjill",
-        "domain": "aol.com",
-        "disposable": False,
-        "toxic": False,
-        "firstname": "Jill",
-        "lastname": "Stein",
-        "gender": "female",
-        "location": None,
-        "country": "United States",
-        "region": "Florida",
-        "city": "West Palm Beach",
-        "zipcode": "33401",
-        "creationdate": None,
-        "processedat": "2017-04-01 02:48:02.592"
-    }
-
+         "address":"flowerjill@aol.com",
+         "status":"valid",
+         "sub_status":"",
+         "free_email":true,
+         "did_you_mean":null,
+         "account":"flowerjill",
+         "domain":"aol.com",
+         "domain_age_days": "8426",
+         "smtp_provider":"yahoo",
+         "mx_record":"mx-aol.mail.gm0.yahoodns.net",
+         "mx_found": true,
+         "firstname":"Jill",
+         "lastname":"Stein",
+         "gender":"female",
+         "country":"United States",
+         "region":"Florida",
+         "city":"West Palm Beach",
+         "zipcode":"33401",
+         "processed_at":"2017-04-01 02:48:02.592"
+        }
 
 @pytest.fixture
 def zerobounce_response_getcredits():
