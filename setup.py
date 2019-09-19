7c8c26d6d797f3bd6a315a5d69f847094de1700b
Swen Kooij <swen@sectorlabs.ro>
2019-09-20 00:20:41 +0300

    Remove dependency on pyandoc
    At the moment, users of the package are required
    to install pypandoc. It is only used to convert
    the README to RST. However, PyPi supports Markdown
    at the moment.

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
-import pypandoc
+
+with open('README.md', 'r') as fp:
+    README = fp.read()
 
 setup(
     name='zerobounce',
@@ -11,9 +13,10 @@ setup(
     author='Tudor Aursulesei',
     author_email='tudor@zerobounce.net',
     url='http://github.com/zerobounce/zerobounce-python-api',
-    long_description = pypandoc.convert("README.md", "rst"),
-    download_url = 'https://github.com/zerobounce/zerobounce-python-api/archive/0.1.4.tar.gz', # I'll explain this in a second
-    keywords = ['email', 'validation'], # arbitrary keywords
+    long_description=README,
+    long_description_content_type="text/markdown",
+    download_url'https://github.com/zerobounce/zerobounce-python-api/archive/0.1.4.tar.gz', # I'll explain this in a second
+    keywords['email', 'validation'], # arbitrary keywords
     packages=[
         'zerobounce',
     ],
